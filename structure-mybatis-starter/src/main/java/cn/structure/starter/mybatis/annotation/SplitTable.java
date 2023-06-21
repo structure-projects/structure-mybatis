aa1b5c636e91b5da3186b25a54c843a4b43027d6
cqliut <cqliut@isoftstone.com>
2023-06-21 18:37:57 +0800

    feat:升级版本调整版本号为1.2.1

--- structure-mybatis-starter/src/main/java/cn/structure/starter/mybatis/annotation/SplitTable.java
+++ structure-mybatis-starter/src/main/java/cn/structure/starter/mybatis/annotation/SplitTable.java
@@ -22,7 +22,7 @@ public @interface SplitTable {
     /**
      * 表名
      *
-     * @return
+     * @return 表名
      */
     String tableName();
 
@@ -30,14 +30,14 @@ public @interface SplitTable {
      * 分表方式，如 time：表示按时间分表
      * key：表示按字段取模分表
      *
-     * @return
+     * @return 返回分表的类型
      */
     SplitTableEnum splitType();
 
     /**
      * 根据什么字段分表
      *
-     * @return
+     * @return 字段
      */
     String splitBy();
 
@@ -45,7 +45,7 @@ public @interface SplitTable {
      * 分表参数，分表方式为time: 表示用于分表的参数名（如 dateInterval）
      * 分表方式为key: 表示取模的基数（如 %32 ，%64）
      *
-     * @return
+     * @return 分表参数
      */
     int keySplitParam() default 64;
 
